215b84600fe5653c5a52c81aa94382f59d19afc9
Andrew D. Horchler <adh9@case.edu>
2013-05-04 12:55:09 -0400

    Improved ISDIAG private function
    sde_isdiag() greatly sped up (maybe 20x) -at least for large matrices.
    
    Signed-off-by: Andrew D. Horchler <adh9@case.edu>

--- SDETools/private/sde_isdiag.m
+++ SDETools/private/sde_isdiag.m
@@ -1,41 +1,41 @@
 function bool=sde_isdiag(V,k)
 %SDE_ISDIAG  True for diagonal matrices.
+%   ISDIAG(V) returns logical 1 (true) if V is a diagonal matrix and logical 0
+%   (false) otherwise. A diagonal matrix is a non-empty square matrix with
+%   arbitrary entries along the main diagonal and is zero elsewhere.
 %
-%   See also: SDE_ISMATRIX, SDE_ISSQUARE
+%   ISDIAG(V,K) returns logical 1 (true) if V is diagonal along the K-th
+%   diagonal and logical 0 (false) otherwise. K > 0 and K < 0 are above and
+%   below the main diagonal, respectively. ISDIAG(V,0) is the same as ISDIAG(V).
+%
+%   See also: SDE_ISMATRIX, SDE_ISSQUARE, DIAG
+
+%   http://mathworks.com/matlabcentral/newsreader/view_thread/299383#806642
 
 %   Andrew D. Horchler, adh9 @ case . edu, Created 4-30-13
-%   Revision: 1.0, 4-30-13
+%   Revision: 1.0, 5-4-13
 
 
-if ~isempty(V) && ndims(V) == 2	%#ok<ISMAT>
+bool = false;
+if ~isempty(V)
     [m,n] = size(V);
-    if m == n
+    if ndims(V) == 2 && m == n	%#ok<ISMAT>
         if nargin == 2
-            if ~isscalar(k) || k ~= floor(k)
+            if ~isscalar(k) || ~isnumeric(k) || any(k ~= floor(k))
                 error('SDETools:sde_isdiag:kthDiagInputNotInteger',...
-                      'K-th diagonal input must be an integer scalar.')
+                      'K-th diagonal input must be a scalar integer.')
             end
-            if abs(k) < n
-                if k < 0
-                    bool = all(V(mod(1-k*n:end,n+1) ~= 1) == 0);
-                else
-                    bool = all(V(mod(1+k:end,n+1) ~= 1) == 0);
-                end
-                if issparse(V)
-                    bool = full(bool);
-                end
-            else
-                bool = false;
+            if isscalar(V)
+                bool = (k == 0);
+            elseif abs(k) < n
+                bool = (nnz(V) == nnz(diag(V,k)));
             end
         else
-            bool = all(V(mod(1:end,n+1) ~= 1) == 0);
-            if issparse(V)
-                bool = full(bool);
+            if isscalar(V)
+                bool = true;
+            else
+                bool = (nnz(V) == nnz(diag(V)));
             end
         end
-    else
-        bool = false;
     end
-else
-    bool = false;
 end
