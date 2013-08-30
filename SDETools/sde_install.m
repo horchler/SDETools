abd680bb028401f9fe9946c50c2044ce0753f5a9
Andrew D. Horchler <adh9@case.edu>
2013-08-30 12:37:36 -0400

    install renamed to sde_install
    more distinctive name to prevent issue is more than one "install" function on path
    
    Signed-off-by: Andrew D. Horchler <adh9@case.edu>

--- SDETools/sde_install.m
+++ SDETools/sde_install.m
@@ -1,16 +1,16 @@
-function install(opt)
-%INSTALL  Add or remove SDETools from Matlab search path and save path.
-%   INSTALL adds the SDETools directory (where this function is located) to the
-%   Matlab search path, saves the path, and prints the help for the toolbox.
+function sde_install(opt)
+%SDE_INSTALL  Add or remove SDETools from Matlab search path and save path.
+%   SDE_INSTALL adds the SDETools directory (where this function is located) to
+%   the Matlab search path, saves the path, and prints the help for the toolbox.
 %
-%   INSTALL('remove') uninstalls SDETools by removing the SDETools directory
+%   SDE_INSTALL('remove') uninstalls SDETools by removing the SDETools directory
 %   from the Matlab search path and saving the path. If SDETools is not
 %   installed (on the path), a warning is issued.
 %
 %   See also path, addpath, rmpath, savepath
 
 %   Andrew D. Horchler, adh9 @ case . edu, Created 8-12-13
-%   Revision: 1.2, 8-12-13
+%   Revision: 1.2, 8-30-13
 
 
 [success,msg] = fileattrib;
@@ -22,16 +22,17 @@ if success
         rmpath(msg.Name);
         status = false;
     else
-        error('SDETools:install:UnknownOption',...
+        error('SDETools:sde_install:UnknownOption',...
               'Input argument must be the string ''remove'' to uninstall.');
     end
 else
-    error('SDETools:install:AddPathError',...
+    error('SDETools:sde_install:AddPathError',...
           'Unable to find absolute path of this directory.');
 end
 
 if savepath
-    error('SDETools:install:SavePathError','Unable to save pathdef.m file.');
+    error('SDETools:sde_install:SavePathError',...
+          'Unable to save pathdef.m file.');
 end
 rehash('toolbox');
 
